d99b441946b610286f763d6db2366c535abceed1
marcotrunc <marcotrunc@gmail.com>
2024-04-18 10:34:46 +0200

    implemented base generic repository

---
+++ Troonch.DataAccess.Base/Repositories/BaseRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Troonch.Domain.Base.Entities;
+
+namespace Troonch.DataAccess.Base.Repositories
+{
+    public abstract class BaseRepository<TEntity, TDbContext> : IBaseRepository<TEntity> where TEntity : BaseEntity
+        where TDbContext : DbContext
+    {
+        protected readonly TDbContext _dbContext;
+        protected BaseRepository(TDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+    }
+}
---
+++ Troonch.DataAccess.Base/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using Troonch.Domain.Base.Entities;
+
+namespace Troonch.DataAccess.Base.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task AddAsync(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
