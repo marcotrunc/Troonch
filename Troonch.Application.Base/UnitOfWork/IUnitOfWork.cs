29deed129a6864820c604d6e241f79d576c0c7e8
marcotrunc <marcotrunc@gmail.com>
2024-04-19 23:17:19 +0200

    implemented unit of work pattern

---
+++ Troonch.Application.Base/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troonch.Application.Base.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    Task<bool> CommitAsync(CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync<T>(T entity, CancellationToken cancellationToken = default) where T : class;
+}
---
+++ Troonch.Application.Base/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Troonch.Application.Base.UnitOfWork
+{
+    public class UnitOfWork<TDbContext> : IUnitOfWork where TDbContext : DbContext
+    {
+        private readonly TDbContext _dbcontext;
+
+        public UnitOfWork(TDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<bool> CommitAsync(CancellationToken cancellationToken)
+        {
+            SetTimestamps();
+            return await _dbcontext.SaveChangesAsync(cancellationToken) > 0;
+        }
+
+        public async Task<bool> UpdateAsync<T>(T entity, CancellationToken cancellationToken = default) where T : class
+        {
+            _dbcontext.Attach(entity);
+
+            _dbcontext.Entry(entity).State = EntityState.Modified;
+
+            return await this.CommitAsync(cancellationToken);
+        }
+
+        private void SetTimestamps()
+        {
+            var entities = _dbcontext.ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+
+            var currentTime = DateTime.UtcNow;
+
+            foreach ( var entity in entities)
+            {
+                try
+                {
+                    if (entity.Property("CreatedOn") == null)
+                    {
+                        continue;
+                    }
+                    if (entity.State == EntityState.Added)
+                    {
+                        entity.Property("CreatedOn").CurrentValue = currentTime;
+                    }
+
+                    if (entity.Property("UpdatedOn") == null)
+                    {
+                        continue;
+                    }
+
+                    entity.Property("UpdatedOn").CurrentValue = currentTime;
+                }
+                catch(Exception ex ) 
+                {
+                    continue;
+                }
+            }
+
+        }
+    }
+}
