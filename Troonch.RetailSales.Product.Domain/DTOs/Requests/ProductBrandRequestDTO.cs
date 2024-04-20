eef2213ff3525b228000f2d4cf8432c83f61f8be
marcotrunc <marcotrunc@gmail.com>
2024-04-20 22:00:31 +0200

    updated brand domain models

---
+++ Troonch.RetailSales.Product.Domain/DTOs/Requests/ProductBrandRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Troonch.RetailSales.Product.Domain.DTOs.Requests
+{
+    public class ProductBrandRequestDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Troonch.RetailSales.Product.Domain/Entities/ProductBrand.cs
+++ Troonch.RetailSales.Product.Domain/Entities/ProductBrand.cs
@@ -7,6 +7,7 @@ namespace Troonch.Sales.Domain.Entities
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Slug { get; set; }
         #region Navigation Properties
         public ICollection<Product> Products { get; set; } = new List<Product>();
         #endregion
