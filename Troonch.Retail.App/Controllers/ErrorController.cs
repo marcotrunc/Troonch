4b03529450e33a24c39fdb432c1b029813f030bf
marcotrunc <marcotrunc@gmail.com>
2024-04-30 19:28:28 +0200

    Added Error Handler

---
+++ Troonch.Retail.App/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Troonch.Retail.App.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statuscode}")]
+        public IActionResult HttpStatusCodeHandler(int statuscode)
+        {
+            ViewBag.StatusCode = statuscode;
+            
+            switch (statuscode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
+                    break;
+
+                case 500:
+                    ViewBag.ErrorMessage = "Internal server error!";
+                    break;
+                
+                default:
+                    ViewBag.ErrorMessage = "Generic Error";
+                    break; ;
+            }
+            return View("ErrorPage");
+        }
+    }
+}
---
+++ Troonch.Retail.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Troonch.Retail.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
