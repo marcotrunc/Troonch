78b58bc9b5a33b60a5dc45ca22799a5b50c69f93
marcotrunc <marcotrunc@gmail.com>
2024-04-30 19:27:51 +0200

    added validation utility

---
+++ Troonch.Application.Base/Utilities/FluentValidationUtility.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+
+namespace Troonch.Application.Base.Utilities;
+
+public static class FluentValidationUtility
+{
+    public static ModelStateDictionary SetModelState<T>(this ModelStateDictionary modelStateDictionary, IEnumerable<ValidationFailure> validationFailure, ILogger<T> _logger)
+    {
+        foreach (var failer in validationFailure)
+        {
+            _logger.LogError($"ProductController::Create -> ${failer.ErrorMessage}");
+            modelStateDictionary.AddModelError(failer.PropertyName, failer.ErrorMessage);
+        }
+        return modelStateDictionary;
+    }
+}
