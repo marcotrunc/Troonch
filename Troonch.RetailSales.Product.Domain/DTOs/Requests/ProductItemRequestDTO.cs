11c6cf88291df426395571f6a3aa415765ba8f0f
marcotrunc <marcotrunc@gmail.com>
2024-05-16 15:28:00 +0200

    added product Item Request Model

---
+++ Troonch.RetailSales.Product.Domain/DTOs/Requests/ProductItemRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using Troonch.Domain.Base.Enums;
+
+namespace Troonch.RetailSales.Product.Domain.DTOs.Requests
+{
+    public class ProductItemRequestDTO
+    {
+        public Guid? Id { get; set; } = Guid.Empty;
+        public Guid ProductId { get; set; }
+        public Guid ProductSizeOptionId { get; set; }
+        public Guid ProductColorId { get; set; }
+        public CurrencyBase Currency { get; set; } = CurrencyBase.EUR;
+        public decimal OriginalPrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public string Barcode { get; set; }
+        public int QuantityAvailable { get; set; }
+    }
+}
